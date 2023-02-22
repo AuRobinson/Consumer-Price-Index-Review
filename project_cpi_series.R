a25b24ef36924c7a06b44c30d606db183d0a4a69
Austin Robinson <95049035+AuRobinson@users.noreply.github.com>
2023-02-21 22:13:45 -0700

    Update project_cpi_series.R

--- project_cpi_series.R
+++ project_cpi_series.R
@@ -1,19 +1,71 @@
 library(data.table)
 library(plotly)
 
-cpi_table <- fread("~/FILE PATH/CPI_SeriesReport.txt") # Maybe use BLS' API one of these days
-cpi_table_long <- melt(cpi_table, id.vars = "Year", variable.name = "Month", value.name = "CPI", na.rm = TRUE) # Wide to Long transformation
-month_table <- data.table(index = c(1:12), month = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")) # Month table because I'm lazy
+# Maybe use BLS' API one of these days
+cpi_table <- fread("~/FILE PATH/CPI_SeriesReport.txt")
 
-cpi_table_long <- cpi_table_long[month_table, on = .(Month = month)] # Index the month table for setorder()... plot_ly() does not like unordered data
-cpi_table_long[, Date := as.Date(paste0(Year, "-", index, "-01", origin = "1970-01-01"))] # Define the date
+cpi_table_long[, Date := as.Date(paste0(Year, "-", index, "-01", origin = "1970-01-01"))] 
 setorder(cpi_table_long, Date) # Order above chaos
 cpi_table_long[, Difference := c(NA, diff(CPI))] # Find month over month difference (first obs. is NA)
 
+# Wide to Long transformation
+cpi_table_long <-
+ melt(
+  cpi_table,
+  id.vars = "Year",
+  variable.name = "Month",
+  value.name = "CPI",
+  na.rm = TRUE
+ )
+
+# Month table to join to `cpi_table_long` for simplicity
+month_table <- data.table(
+ index = c(1:12),
+ month = c(
+  "Jan", "Feb", "Mar", "Apr", "May","Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+ )
+)
+
+# Join `cpi_table_long` and `month_table`
+cpi_table_long <- cpi_table_long[month_table, on = .(Month = month)]
+
+# Define [Date] then setorder()... plot_ly() does not like unordered data for line plots
+cpi_table_long[, Date := as.Date(paste0(Year, "-", index, "-01", origin = "1970-01-01"))]
+setorder(cpi_table_long, Date)
+
+# Define [Difference] to month-to-month-changes
+cpi_table_long[, Difference := c(NA, diff(CPI))]
+
+# Visuals ----
+
 # Plot 1: Combo with `Difference` on right-y-axis by date
-plot_ly(data = cpi_table_long[Year >= 1900], x = ~ Date, y = ~ CPI, name = "CPI", type = "scatter", mode = "lines") |>
- add_trace( y = ~ Difference, alpha = 0.5, name = "Difference", type = "scatter", mode = "lines", yaxis = "y2") |>
- layout(yaxis2 = list(title = "Difference", overlaying = "y", side = "right"))
+plot_ly(
+ data = cpi_table_long[Year >= 1900],
+ x = ~ Date,
+ y = ~ CPI,
+ name = "CPI (left)",
+ type = "scatter",
+ mode = "lines"
+) |> 
+ add_trace(
+  y = ~ Difference,
+  alpha = 0.5,
+  name = "Difference (right)",
+  type = "scatter",
+  mode = "lines",
+  yaxis = "y2"
+ ) |>
+ layout(yaxis2 = list(
+  title = "Difference",
+  overlaying = "y",
+  side = "right"
+ ))
 
 # Plot 2: Box with `Difference` to see volatility over time
-plot_ly(data = cpi_table_long[], x = ~ Year, y = ~ Difference, type = 'box')
+plot_ly(
+ data = cpi_table_long,
+ x = ~ Year,
+ y = ~ Difference,
+ type = 'box'
+)
